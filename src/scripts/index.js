3b67594f0d3dbe43c2b02ece0f47abe996a10002
pedrohenriquebarrosn <pedronascimentobr@hotmail.com>
2022-12-22 14:19:28 -0300

    envio

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,61 @@
-/* Monte sua lógica aqui */
+const render = arr => {
+    const mainList = document.querySelector(".main__list")
+
+    mainList.innerHTML = ""
+
+    arr.forEach(element => {
+        const card = createCard(element)
+
+        mainList.appendChild(card)
+    });
+}
+
+const createCard = ({ url, name }) => {
+    const container = document.createElement("li")
+    const image = document.createElement("img")
+    const p = document.createElement("p")
+
+    container.className = "pokemon"
+
+
+    const pokedexNumber = url.slice(34, -1)
+
+    image.className = "pokemon__url"
+    image.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokedexNumber}.png`
+    image.alt = name
+
+    p.className = "pokemon__name"
+    p.innerText = name
+
+    container.append(image, p)
+
+    return container
+}
+
+const createCardSearched = element => {
+    const container = document.createElement("li")
+    const image = document.createElement("img")
+    const p = document.createElement("p")
+
+    container.className = "pokemon"
+
+    image.className = "pokemon__url"
+    image.src = element.sprites.front_default
+    image.alt = element.name
+
+    p.className = "pokemon__name"
+    p.innerText = element.name
+
+    container.append(image, p)
+
+    return container
+}
+
+const createLoading = () => {
+    const span = document.createElement("span")
+
+    span.className = "carregando"
+    span.innerText = "Carregando..."
+
+    return span
+}
--- src/scripts/requests.js
+++ src/scripts/requests.js
@@ -1,1 +1,64 @@
-/* Monte a lógica das requisições aqui */
+const getAllPokemons = async () => {
+    const pokemons = await fetch("https://pokeapi.co/api/v2/pokemon", {
+        method: "GET",
+        headers: {
+            "content-type": "application/json"
+        }
+    })
+        .then(res => res.json())
+        .then(res => {
+            const mainList = document.querySelector(".main__list")
+            const loading = createLoading()
+
+            mainList.innerHTML = ""
+
+            mainList.appendChild(loading)
+
+            render(res.results)
+            return res
+        })
+
+    return pokemons
+}
+getAllPokemons()
+
+const getPokemonByName = async pokemonName => {
+    const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`, {
+        method: "GET",
+        headers: {
+            "content-type": "application/json"
+        }
+    })
+        .then(res => res.json())
+        .then(res => {
+            const mainList = document.querySelector(".main__list")
+            const loading = createLoading()
+
+            mainList.innerHTML = ""
+
+            mainList.appendChild(loading)
+
+            return res
+        })
+
+    return pokemon
+}
+
+const renderSearch = () => {
+    const input = document.querySelector("input")
+    const searchButton = document.querySelector(".search__button")
+
+    searchButton.addEventListener("click", async () => {
+
+        const mainList = document.querySelector(".main__list")
+
+        mainList.innerHTML = ""
+
+        const searchedPokemon = await getPokemonByName(input.value)
+
+        const card = createCardSearched(searchedPokemon)
+
+        mainList.appendChild(card)
+    })
+}
+renderSearch()
